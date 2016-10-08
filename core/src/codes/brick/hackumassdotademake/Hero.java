daad40b49ad731f0dff2ab66e4bf1e53f4a00e60
bwaters <bwaters@umass.edu>
2016-10-08 19:32:51 -0400

    reduce jumping speed

--- core/src/codes/brick/hackumassdotademake/Hero.java
+++ core/src/codes/brick/hackumassdotademake/Hero.java
@@ -11,8 +11,8 @@ public abstract class Hero extends Sprite {
   private int health;
 
   private final int GROUND_Y_VALUE = 0;
-  private final int JUMP_SPEED = 50;
-  private final int JUMP_CHANGE = 1;
+  private final int JUMP_SPEED = 30;
+  private final int JUMP_CHANGE = 2;
   private int currentJumpSpeed = JUMP_SPEED;
 
   private enum State {
@@ -87,10 +87,7 @@ public abstract class Hero extends Sprite {
   public void jump() {
     if (currentJumpState == State.JUMPING)
     {
-      //We're ascending. Decrease the Y coordinate of the sprite by the speed.
-      //      DecreaseYCoordinateBy(Speed);
       this.setY(this.getY()+currentJumpSpeed);
-      //      DecreaseByValue(Speed, Value); //The character needs to jump smoothly, so the speed should decrease as he ascends.
       currentJumpSpeed -= JUMP_CHANGE;
       if (currentJumpSpeed <= 0)
       {
@@ -101,18 +98,12 @@ public abstract class Hero extends Sprite {
     }
     else if (currentJumpState == State.FALLING)
     {
-      //We're descending. Increase the Y coordinate by the speed (at first, it's 0).
-      //      IncreaseYCoordinateBy(Speed);
       this.setY(this.getY()-currentJumpSpeed);
-      //      IncreaseByValue(Speed, Value); //Increase the speed, so the character falls gradually faster.
       currentJumpSpeed += JUMP_CHANGE;
       if (this.getY() == GROUND_Y_VALUE)
       {
-        //If we reached the original Y coordinate, we hit the ground. Mark the character as standing.
-        //           ChangeState(Standing);
         currentJumpState = State.STANDING;
         currentJumpSpeed = JUMP_SPEED;
-        //           CurrentYCoordinate = OriginalYCoordinate;
 
       }
     }
