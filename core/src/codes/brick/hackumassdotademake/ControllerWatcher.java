8b387e682d8a9072379b1f2b486ab6d10b7cd920
Richard McCormack <brick@brick.codes>
2016-10-08 22:03:32 -0400

    ADd contronler

---
+++ core/src/codes/brick/hackumassdotademake/ControllerWatcher.java
@@ -0,0 +1,69 @@
+package codes.brick.hackumassdotademake;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerAdapter;
+import com.badlogic.gdx.controllers.mappings.Xbox;
+
+public class ControllerWatcher extends ControllerAdapter {
+  private Hero player1;
+  private Hero player2;
+
+  public ControllerWatcher(Hero player1, Hero player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+  }
+
+  @Override
+  public boolean buttonDown(Controller controller, int buttonCode) {
+    // TODO: configurable controls
+    if (controller == player1.controller) {
+      if (buttonCode == 0) {
+        player1.upSignal();
+      } else if (buttonCode == 4) {
+        player1.useFirstSpell();
+      } else if (buttonCode == 2) {
+        player1.useSecondSpell();
+      }
+      return true;
+    }
+
+    if (buttonCode == 0) {
+      player2.upSignal();
+    } else if (buttonCode == 4) {
+      player2.useFirstSpell();
+    } else if (buttonCode == 2) {
+      player2.useSecondSpell();
+    }
+    return true;
+  }
+
+  @Override
+  public boolean axisMoved(Controller controller, int axisCode, float value) {
+    // 0 == horizontal Lstick axis on window for xbox360
+    if (axisCode != 0) {
+      return true;
+    }
+
+    if (controller == player1.controller) {
+      if (value < 0) {
+        player1.leftSignal();
+      } else if (value > 0) {
+        player1.rightSignal();
+      } else {
+        player1.stopMotion();
+      }
+      return true;
+    }
+
+    if (value < 0) {
+      player2.leftSignal();
+    } else if (value > 0) {
+      player2.rightSignal();
+    } else {
+      player2.stopMotion();
+    }
+
+    return true;
+  }
+}
